b0e77b3a69f4ef9f5e1adebf199bf1b9e96ad002
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-03-31 21:41:44 +0700

    user index page is set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: [:user, :editor, :admin]
+  
   after_initialize :set_default_role, if: :new_record?
 
   def set_default_role
