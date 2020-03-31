4ec59519cf1f7236aec425b7cef4b99b200b9622
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-03-31 21:51:25 +0700

    add application controller pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  rescure_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  protect_from_forgery with: :exception
+
+  private
+    def user_not_authorized
+      flash[:alert] = "Access denied."
+      redirect_to (request.reffer || root_path)
+    end
 end
