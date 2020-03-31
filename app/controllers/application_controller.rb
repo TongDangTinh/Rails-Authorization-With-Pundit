5e9f4c435339a592cdd7a18e1adfd6326942af17
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-01 01:02:00 +0700

    Add headeer, some controller users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  rescure_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
 
   private
     def user_not_authorized
       flash[:alert] = "Access denied."
-      redirect_to (request.reffer || root_path)
+      redirect_to root_path
     end
 end
